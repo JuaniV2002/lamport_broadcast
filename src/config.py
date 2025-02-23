0023e9891ed7f00df8b006b3ebbb346f87762179
JuaniV2002 <juani2002villa@icloud.com>
2025-02-23 20:42:12 -0300

    arreglo en eleccion de procesos

--- src/config.py
+++ src/config.py
@@ -1,9 +1,12 @@
-NUM_PROCESSES = 3  # Change this value to support any number of processes
+NUM_PROCESSES = 10  # Change this value as needed
 BASE_PORT = 5000
 
-# Automatically generate process configurations for p1, p2, ..., pN.
+# Determine the number of digits for zero-padding based on NUM_PROCESSES.
+num_digits = len(str(NUM_PROCESSES))
+
+# Automatically generate process configurations for p01, p02, ..., pN.
 PROCESSES = {
-    f'p{i+1}': {'host': 'localhost', 'port': BASE_PORT + i}
+    f'p{str(i+1).zfill(num_digits)}': {'host': 'localhost', 'port': BASE_PORT + i}
     for i in range(NUM_PROCESSES)
 }
 
